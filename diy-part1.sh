4c63885f7462f6edc23aa5c2bf1344057a5f8f12
kkstone <10545609+kkstone@users.noreply.github.com>
2024-09-14 10:47:45 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -12,4 +12,12 @@
 # Uncomment a feed source
 # sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
+cd package
+git clone https://github.com/kenzok8/small.git
+rm -rf small/tcping
+git clone https://github.com/kuoruan/openwrt-frp.git
+git clone https://github.com/xiaoxiao29/luci-app-adguardhome.git
+git clone https://github.com/jerrykuku/lua-maxminddb.git
 
+wget https://github.com/kkstone/Actions-RAX3000M-NAND-EMMC/raw/emmc/luci-packages/luci-app-frp.zip && unzip luci-app-frp.zip && rm -rf luci-app-frp.zip
+wget https://github.com/kkstone/Actions-RAX3000M-NAND-EMMC/raw/emmc/luci-packages/luci-app-wolplus.zip && unzip luci-app-wolplus.zip && rm -rf luci-app-wolplus.zip
