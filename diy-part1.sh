a15b0bf1ce11c194ad3469d2b65fe55c35e38363
kkstone <10545609+kkstone@users.noreply.github.com>
2025-01-15 17:11:40 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -13,8 +13,8 @@
 # sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
 cd package
-git clone https://github.com/kenzok8/small.git
-rm -rf small/tcping
+git clone https://github.com/xiaorouji/openwrt-passwall.git
+git clone https://github.com/xiaorouji/openwrt-passwall-packages.git
 git clone https://github.com/xiaoxiao29/luci-app-adguardhome.git
 git clone https://github.com/jerrykuku/lua-maxminddb.git
 
