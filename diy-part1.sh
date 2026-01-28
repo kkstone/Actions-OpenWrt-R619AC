039a8ef2f2c627af84f4ad08ffe265793259728d
kkstone <10545609+kkstone@users.noreply.github.com>
2026-01-28 21:09:08 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -13,8 +13,8 @@
 # sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
 cd package
-git clone https://github.com/xiaorouji/openwrt-passwall.git
-git clone https://github.com/xiaorouji/openwrt-passwall-packages.git
+git clone https://github.com/Openwrt-Passwall/openwrt-passwall.git
+git clone https://github.com/Openwrt-Passwall/openwrt-passwall-packages.git
 git clone https://github.com/xiaoxiao29/luci-app-adguardhome.git
 git clone https://github.com/jerrykuku/lua-maxminddb.git
 
